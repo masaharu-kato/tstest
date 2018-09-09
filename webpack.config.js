42556d8a253a0108aad35099e7ebcb1804b46b6f
kato <fudai.mk@gmail.com>
2018-09-09 15:08:35 +0900

    improved graph test

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
 const path = require('path');
 module.exports = {
+
     // モジュールバンドルを行う起点となるファイルの指定
     // 指定できる値としては、ファイル名の文字列や、それを並べた配列やオブジェクト
     // 下記はオブジェクトとして指定した例 
     entry: {
         bundle: './src/app.ts'
     },  
+
     output: {
         // モジュールバンドルを行った結果を出力する場所やファイル名の指定
         // "__dirname"はこのファイルが存在するディレクトリを表すnode.jsで定義済みの定数
         path: path.join(__dirname,'dist'),
         filename: '[name].js'  // [name]はentryで記述した名前(この例ではbundle）が入る
     },
+
+    // ここからチャンクの設定
+    optimization: {
+        splitChunks: {
+            name: 'vendor',
+            chunks: 'initial',
+        }
+        },
+        // ここまでチャンクの設定
+
     // モジュールとして扱いたいファイルの拡張子を指定する
     // 例えば「import Foo from './foo'」という記述に対して"foo.ts"という名前のファイルをモジュールとして探す
     // デフォルトは['.js', '.json']
     resolve: {
         extensions:['.ts','.js']
     },
+
     devServer: {
         // webpack-dev-serverの公開フォルダ
         contentBase: path.join(__dirname,'dist')
     },
+
     // モジュールに適用するルールの設定（ここではローダーの設定を行う事が多い）
     module: {
         rules: [
@@ -31,4 +45,5 @@ module.exports = {
             }
         ]
     }
+    
 }
