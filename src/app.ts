8f4e1348121f6ac58f435ab7d24670cec612cdde
kato <fudai.mk@gmail.com>
2018-09-09 14:56:12 +0900

    graph test

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
-import {Item} from './item';
-var elem = document.getElementById('output');
-var aBook = new Item('はじめてのTypeScript',1980);
-aBook.say(elem);
+import * as c3  from 'c3';
+
+let chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        type: 'donut',
+        columns: [
+            ['良い感じ', 50],
+            ['ピンとこない', 50]  
+        ]
+    },
+    donut: {
+        title: 'TypeScriptの印象は？'
+    }
+});
